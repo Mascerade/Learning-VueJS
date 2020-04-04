a51dc62b2195bbe5b1cd9db455879682e5d8f059
Jason Acheampong <Ultimi450@gmail.com>
2020-04-04 11:13:24 -0400

    got the progress bar working in the quotes application

---
+++ Quotes Application/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <app-progressbar></app-progressbar>
+    </div>
+</template>
+
+<script>
+    import ProgressBar from './components/ProgressBar.vue'
+    export default {
+        components: {
+            'app-progressbar': ProgressBar
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Quotes Application/src/components/ProgressBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>Quotes Added</h2>
+        <div id="progress-container">
+            <div id="inner-progress" :style="{ width: quotesAdded * 10 + '%' }">
+                {{quotesAdded}} / 10
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            quotesAdded: 1
+        }
+    }
+}
+</script>
+
+<style>
+    #progress-container {
+        width: 100%;
+        height: 20px;
+        border-radius: 10px;
+        background-color:#f5f5f5;
+    }
+
+    #inner-progress {
+        height: 100%;
+        width: 10%;
+        border-radius: 10px;
+        background-color:#66b5f2;
+        text-align: center;
+        color: white;
+    }
+</style>
