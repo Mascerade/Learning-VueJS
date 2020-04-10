9228fe5b3ae50e998d617d9418a40150785a4c66
Jason Acheampong <Ultimi450@gmail.com>
2020-04-10 10:56:03 -0400

    finished directives exercise

--- Directives-Exercise/src/App.vue
+++ Directives-Exercise/src/App.vue
@@ -5,15 +5,40 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <div v-myon:mousemove="justPrint" id="someting">
+                    Big Div
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            'myon': {
+                bind(el, binding, vnode) {
+                    el.addEventListener(binding.arg, binding.value)
+                }
+            }
+        },
+        
+        methods: {
+            justPrint() {
+                console.log("Event fired")
+            }
+        }
+    }
 </script>
 
 <style>
+#someting {
+    display: flex;
+    padding: 100px;
+    background-color: red;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    font-size: 30px
+}
 </style>
